f2bf570d1445a7d41a42f151d837107ddfddc972
Allysa <334843652@qq.com>
2015-11-27 11:33:52 +0800

    first issue

---
+++ calculate-first.js
@@ -0,0 +1,18 @@
+function single_element(arr) {
+{
+    var collection_a = [1, 2, 3, 2, 5, 6, 21, 43, 12, 5];
+    var a={};
+    for (var i=1;i<collection_a.length;i+2)
+    {
+      var v=collection_a[i];
+    }
+    for(var i=0;i<v.length;i++)
+    {
+      var x=v[i];
+      if (typeof(a[x])=='undefined')
+      {a[v]=1;}
+
+    };
+    return x;
+
+}
